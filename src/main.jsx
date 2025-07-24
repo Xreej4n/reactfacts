04092f6e3d1d27469b87ac7319ddc6c17a137fcb
Xreej4n <sreejannath1@gmail.com>
2025-07-24 23:31:35 +0530

    Base commit 	new file:   .gitignore 	new file:   README.md 	new file:   eslint.config.js 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/assets/react.svg 	new file:   src/index.css 	new file:   src/main.jsx 	new file:   vite.config.js

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { createRoot } from "react-dom/client"
+createRoot(document.getElementById("root")).render(
+  <div>
+    <img height="55px" src="/src/assets/react.svg"></img>
+    <h1>Fun Facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally by Jordan Walke</li>
+      <li>Has well over 100K stars on GitHub</li>
+      <li>Is maintained by Meta</li>
+      <li>Powers thousands of enterprise apps, including
+mobile apps</li>
+
+    </ul>
+  </div>
+)
